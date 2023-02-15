b252b4b52bb136ef5bf8c2253680aaae89d8ede3
Hassan <hassanch5209@gmail.com>
2023-02-15 20:50:45 +0500

    first commit

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+// import './App.css';
+import Typography from "./components/Typography";
+
+function App() {
+  return (
+    <>
+      <Typography></Typography>
+    </>
+  );
+}
+
+export default App;
---
+++ app/src/components/Typography.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Typography from '@mui/material/Typography';
+
+export default function Typo() {
+    return (
+        <>
+            <Typography>
+                Typography
+            </Typography>
+
+            <Typography variant='h2' align="right" style={{border:'2px solid red'}}>
+                Typography
+            </Typography>
+        </>
+    )
+}
